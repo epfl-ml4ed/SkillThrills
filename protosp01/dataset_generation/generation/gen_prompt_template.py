0fbff43b423f96a458b11a137a868e4c099bec78
Antoine Jacques Alain Magron <magron@iccluster130.iccluster.epfl.ch>
2023-12-06 12:53:07 +0100

    adds modification to prompt

--- protosp01/dataset_generation/generation/gen_prompt_template.py
+++ protosp01/dataset_generation/generation/gen_prompt_template.py
@@ -59,14 +59,14 @@ PROMPT_TEMPLATE = {
             
         ]
     },
-    "PROTO-GEN-A0":{ ## Used for prototype to make sure we get only one sample
+    "PROTO-GEN-A0":{ ## DENSE
         "role_instruction": "You are the leading AI Writer at a large, multinational HR agency. You are considered as the world's best expert at expressing required skills and knowledge in a variety of clear ways. You are particularly proficient with the ESCO Occupation and Skills framework. As you are widely lauded for your job posting writing ability, you will assist the user in all job-posting, job requirements and occupational skills related tasks.\n",
-        "instruction": "You work in collaboration with ESCO to gather rigid standards for job postings. Given a list of ESCO skills and knowledges, you're asked to produce a single example of exactly one sentence that could be found in a job ad and refer to all skill or knowledge component. Ensure that your sentence is well written and could be found in real job advertisement. Use a variety of styles. You're trying to provide a representative sample of the many, many ways real job postings would evoke skills. All the skills in : {skillList} must be integrated into each sentences. A candidate should have different degrees of expertise in all the given skills. This degree should be specified for each skills in the sentence. You must not include any skills in ESCO that were not given to you. Try to be as implicit as possible when mentionning the skill. Try not to use the exact skill string. {wordsToAvoid}. Avoid explicitly using the wording of this extra information in your examples.\n",
+        "instruction": "You work in collaboration with ESCO to gather rigid standards for job postings. Given a list of ESCO skills and knowledges, you're asked to produce a single example of exactly one sentence that could be found in a job ad and refer to all skill or knowledge component. Ensure that your sentence is well written and could be found in real job advertisement. Use a variety of styles. You're trying to provide a representative sample of the many, many ways real job postings would evoke skills. All the skills in : {skillList} must be integrated. A candidate should have different degrees of expertise in all the given skills. This degree should be specified for each skills in the sentence. You must not include any skills in ESCO that were not given to you. Try to be as implicit as possible when mentionning the skill. Try not to use the exact skill string. {wordsToAvoid}. Avoid explicitly using the wording of this extra information in your examples. Your sentence must not start with 'We are seeking', 'We are looking' or 'We are searching'. Generate stricly only one example.\n",
         "shots": [
             
         ]
     },
-    "PROTO-GEN-A1":{
+    "PROTO-GEN-A1":{ ## SPARSE
         "role_instruction": "You are the leading AI Writer at a large, multinational HR agency. You are considered as the world's best expert at expressing required skills and knowledge in a variety of clear ways. You are particularly proficient with the ESCO Occupation and Skills framework. As you are widely lauded for your job posting writing ability, you will assist the user in all job-posting, job requirements and occupational skills related tasks.\n",
         "instruction": "You work in collaboration with ESCO to gather rigid standards for job postings. Given a list of ESCO skills and knowledges, you're asked to provide a single paragraph of a few lines that could be found in a job ad and refer to all skill or knowledge component. You may be given a skill family to help you disambiguate if the skills names could refer to multiple things. Ensure that your paragraphs are well written and could be found in real job advertisement. Use a variety of styles. Write paragraphs of a few sentences. You're trying to provide a representative sample of the many, many ways real job postings would evoke skills. All the skills must be integrated into each paragraph. A candidate should have different degrees of expertise in all the given skills. This degree should be specified. You must not integrate any skill not given in input to the paragraph. The references to the skills must be as implicit as possible and must thus not contain the given skill string. {wordsToAvoid} Avoid explicitly using the wording of this extra information in your examples.\n",
         "shots": [
@@ -90,7 +90,23 @@ PROMPT_TEMPLATE = {
             ]
 
         }
+    },
+
+
+    ####################################################################################
+    "PAPERV2":{
+        "system": "Respond with sentences from hypothetical job ads that require a certain skill, as asked by the user.",
+        "Instruction": "Respond with sentences from hypothetical job ads that require a certain skill, as asked by the user.",
+        "shots":[
+            
+        ]
+
     }
+
+
+    ####################################################################################
+
+
 }
 
 
