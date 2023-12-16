a820ff205b335cf728c16bd101947ad38fafb597
Antoine Jacques Alain Magron <magron@iccluster130.iccluster.epfl.ch>
2023-12-16 21:55:53 +0100

    add modif to utils

--- protosp01/skillExtract/utils.py
+++ protosp01/skillExtract/utils.py
@@ -626,6 +626,93 @@ def get_top_vec_similarity(
     )
     return emb_tax
 
+## BASE VERSION
+# def select_candidates_from_taxonomy(
+#     sample,
+#     taxonomy,
+#     splitter,
+#     model,
+#     tokenizer,
+#     max_candidates=10,
+#     method="rules",
+#     emb_tax=None,
+# ):
+#     assert method in ["rules", "embeddings", "mixed"]
+#     sample["skill_candidates"] = {}
+#     if len(sample["extracted_skills"]) > 0:
+#         for extracted_skill in sample["extracted_skills"]:
+#             #print("extracted skill:", extracted_skill)
+
+#             if method == "rules" or method == "mixed":
+#                 # print("checking for matches in name+definition")
+#                 taxonomy["results"] = taxonomy["name+definition"].str.contains(
+#                     extracted_skill, case=False, regex=False
+#                 )
+
+#                 if not taxonomy["results"].any():
+#                     # print("checking for matches in example")
+#                     taxonomy["results"] = taxonomy["Example"].str.contains(
+#                         extracted_skill, case=False, regex=False
+#                     )
+
+#                 if not taxonomy["results"].any():
+#                     # print("checking for matches in subwords")
+#                     taxonomy["results"] = False
+#                     for subword in filter_subwords(extracted_skill, splitter):
+#                         taxonomy["results"] = taxonomy["results"] + taxonomy[
+#                             "name+definition"
+#                         ].str.contains(subword, case=False, regex=False)
+
+#                 # if not taxonomy["results"].any():
+#                 #     if method == "rules":
+#                 #         # print("checking for matches in difflib")
+#                 #         matching_elements = difflib.get_close_matches(
+#                 #             extracted_skill,
+#                 #             taxonomy["name+definition"],
+#                 #             cutoff=0.4,
+#                 #             n=max_candidates,
+#                 #         )
+#                 #         taxonomy["results"] = taxonomy["name+definition"].isin(
+#                 #             matching_elements
+#                 #         )
+
+#                 if not taxonomy["results"].any():
+#                     print("No candidates found for: ", extracted_skill)
+
+#                 if taxonomy["results"].sum() > 10:
+#                     true_indices = taxonomy.index[taxonomy["results"]].tolist()
+#                     selected_indices = np.random.choice(true_indices, 10, replace=False)
+#                     taxonomy["results"] = False
+#                     taxonomy.loc[selected_indices, "results"] = True
+
+#             if method == "embeddings" or method == "mixed":
+#                 # print("checking for highest embedding similarity")
+#                 emb_tax = get_top_vec_similarity(
+#                     extracted_skill,
+#                     sample["sentence"],
+#                     emb_tax,
+#                     model,
+#                     tokenizer,
+#                     max_candidates,
+#                 )
+#                 if method == "embeddings":
+#                     taxonomy["results"] = emb_tax["results"]
+#                 else:
+#                     taxonomy["results"] = taxonomy["results"] | emb_tax["results"]
+#             # if taxonomy["results"].sum() > 0:
+#             #     breakpoint()
+
+#             keep_cols = [
+#                 "unique_id",
+#                 "Type Level 2",
+#                 "name+definition",
+#             ]
+
+#             matching_df = taxonomy[taxonomy["results"] == True][keep_cols]
+
+#             sample["skill_candidates"][extracted_skill] = matching_df.to_dict("records")
+
+#     return sample
 
 def select_candidates_from_taxonomy(
     sample,
@@ -641,50 +728,39 @@ def select_candidates_from_taxonomy(
     sample["skill_candidates"] = {}
     if len(sample["extracted_skills"]) > 0:
         for extracted_skill in sample["extracted_skills"]:
-            #print("extracted skill:", extracted_skill)
+            # print("extracted skill:", extracted_skill)
 
+            # NOTE: this is to handle rule-based/string-matching ways of selecting candidates
             if method == "rules" or method == "mixed":
-                # print("checking for matches in name+definition")
-                taxonomy["results"] = taxonomy["name+definition"].str.contains(
+                taxonomy["results"] = False
+                # we will check for exact matches first = 100% skill in name+definition
+                taxonomy["match_pct"] = taxonomy["name+definition"].str.contains(
                     extracted_skill, case=False, regex=False
+                ).astype(int)
+                taxonomy["match_pct"] = taxonomy["match_pct"].astype(int)
+                taxonomy["match_type"] = np.where(
+                    taxonomy["match_pct"] == 1, "exact", "none"
                 )
 
-                if not taxonomy["results"].any():
-                    # print("checking for matches in example")
-                    taxonomy["results"] = taxonomy["Example"].str.contains(
-                        extracted_skill, case=False, regex=False
+                if (taxonomy["match_pct"] == 1).any():
+                    if sum(taxonomy["match_pct"] == 1) <= max_candidates:
+                        taxonomy["results"] = taxonomy["match_pct"].astype(bool)
+                    else:
+                        # randomly select max_candidates from the exact matches
+                        taxonomy["results"] = taxonomy["match_pct"].sample(
+                            n=max_candidates, random_state=42
+                        ).astype(bool)
+                else:
+                    taxonomy["match_pct"] = taxonomy["name"].apply(
+                        lambda x: fuzz.token_set_ratio(extracted_skill, x)
                     )
+                    # take the top max_candidates
+                    taxonomy["results"] = taxonomy["match_pct"].rank(
+                        method="first", ascending=False
+                    ) <= max_candidates
+                    taxonomy["match_type"] = "fuzzy"
 
-                if not taxonomy["results"].any():
-                    # print("checking for matches in subwords")
-                    taxonomy["results"] = False
-                    for subword in filter_subwords(extracted_skill, splitter):
-                        taxonomy["results"] = taxonomy["results"] + taxonomy[
-                            "name+definition"
-                        ].str.contains(subword, case=False, regex=False)
-
-                # if not taxonomy["results"].any():
-                #     if method == "rules":
-                #         # print("checking for matches in difflib")
-                #         matching_elements = difflib.get_close_matches(
-                #             extracted_skill,
-                #             taxonomy["name+definition"],
-                #             cutoff=0.4,
-                #             n=max_candidates,
-                #         )
-                #         taxonomy["results"] = taxonomy["name+definition"].isin(
-                #             matching_elements
-                #         )
-
-                if not taxonomy["results"].any():
-                    print("No candidates found for: ", extracted_skill)
-
-                if taxonomy["results"].sum() > 10:
-                    true_indices = taxonomy.index[taxonomy["results"]].tolist()
-                    selected_indices = np.random.choice(true_indices, 10, replace=False)
-                    taxonomy["results"] = False
-                    taxonomy.loc[selected_indices, "results"] = True
-
+            # NOTE: this is to handle embedding-based ways of selecting candidates
             if method == "embeddings" or method == "mixed":
                 # print("checking for highest embedding similarity")
                 emb_tax = get_top_vec_similarity(
@@ -695,6 +771,8 @@ def select_candidates_from_taxonomy(
                     tokenizer,
                     max_candidates,
                 )
+
+                # NOTE: below is to either use the embeddings or a mix of embeddings and rules
                 if method == "embeddings":
                     taxonomy["results"] = emb_tax["results"]
                 else:
@@ -705,6 +783,7 @@ def select_candidates_from_taxonomy(
             keep_cols = [
                 "unique_id",
                 "Type Level 2",
+                "name",
                 "name+definition",
             ]
 
